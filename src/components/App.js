6416468ecf970d61ee4cb221cd7ea1d7a57ad868
Vikas Kumar <vk972832@gmail.com>
2025-04-28 15:21:40 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,42 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import Review from "./Review";
 
+function App() {
+  // inline styles
+  const styles = {
+    main: {
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#f1f5f8",
+      padding: "2rem",
+    },
+    container: {
+      background: "#fff",
+      borderRadius: "0.5rem",
+      padding: "2rem",
+      maxWidth: "400px",
+      textAlign: "center",
+      boxShadow: "0px 5px 15px rgba(0,0,0,0.1)",
+    },
+    heading: {
+      marginBottom: "1.5rem",
+      fontSize: "2rem",
+      color: "#102a42",
+    },
+  };
+
+  return (
+    <main style={styles.main}>
+      <section className="container" style={styles.container}>
+        <h2 id="review-heading" style={styles.heading}>
+          Our Reviews
+        </h2>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
